9dcfc343c70c8dff719a264ad11591d04f895b52
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-10-11 17:01:09 +0300

    Update constructorScript-playlist.js

--- constructorScript-playlist.js
+++ constructorScript-playlist.js
@@ -1,1 +1,5 @@
 document.getElementById("НАСТОЯЩЕЕ ВРЕМЯ").href = "https://rfe-lh.akamaihd.net/i/rfe_tvmc5@383630/master.m3u8";
+document.getElementById("МИН.ИДЕЙ").href = "https://live-minidey.cdnvideo.ru/minidey/minidey.sdp/playlist.m3u8";
+document.getElementById("УНИВЕР СМОТРИ").href = "https://cdn.universmotri.ru/live/hq.sdp/playlist.m3u8";
+document.getElementById("Три Ангела").href = "https://hls.tv.3angels.ru/stream/HQ.m3u8";
+document.getElementById("RU TV МУЗЫКА").href = "https://rutv.gcdn.co/streams/1410_95/playlist.m3u8";
