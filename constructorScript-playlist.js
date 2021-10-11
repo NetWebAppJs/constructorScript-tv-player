2494bcca5e9381118440afdaa7c6d7b2c9c83a3b
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-10-11 16:00:38 +0300

    Update constructorScript-playlist.js

--- constructorScript-playlist.js
+++ constructorScript-playlist.js
@@ -1,1 +1,1 @@
-document.getElementById("image").href = "https://rfe-lh.akamaihd.net/i/rfe_tvmc5@383630/master.m3u8";
+document.getElementById("НАСТОЯЩЕЕ ВРЕМЯ").href = "https://rfe-lh.akamaihd.net/i/rfe_tvmc5@383630/master.m3u8";
