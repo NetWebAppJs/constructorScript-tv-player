67ae8867a3e1ef758b987e133ae5129b675ff1fe
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-10-11 17:24:27 +0300

    Update constructorScript-playlist.js

--- constructorScript-playlist.js
+++ constructorScript-playlist.js
@@ -6,6 +6,6 @@ document.getElementById("RU TV МУЗЫКА").href = "https://rutv.gcdn.co/strea
 document.getElementById("RT DOC ").href = "https://rtmp.api.rt.com/hls/rtdru.m3u8";
 document.getElementById("НТМ").href = "https://live-ntm13.cdnvideo.ru/ntm13/smil:ntm13.smil/playlist.m3u8";
 document.getElementById("Euronews").href = "https://evronovosti.mediacdn.ru/sr1/evronovosti/playlist.m3u8";
-document.getElementById("Три Ангела").href = "https://hls.tv.3angels.ru/stream/HQ.m3u8";
-document.getElementById("RU TV МУЗЫКА").href = "https://rutv.gcdn.co/streams/1410_95/playlist.m3u8";
+/*document.getElementById("Три Ангела").href = "https://hls.tv.3angels.ru/stream/HQ.m3u8";
+document.getElementById("RU TV МУЗЫКА").href = "https://rutv.gcdn.co/streams/1410_95/playlist.m3u8";*/
 
