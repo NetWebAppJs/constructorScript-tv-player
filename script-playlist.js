b5ba7e9c37a4a59ba48977aaa4dadef7026f42f4
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-10-11 16:27:48 +0300

    Update script-playlist.js

--- script-playlist.js
+++ script-playlist.js
@@ -7,13 +7,19 @@ for(var i=0; i<links.length; i++){
 function handler(e){
   e.preventDefault();
   videotarget=this.getAttribute("href");
-  filename=videotarget.substr(0,videotarget.lastIndexOf('.'))||videotarget;
+  
   video=document.querySelector("#video_player video");
   video.removeAttribute('poster');
-  source=document.querySelectorAll("#video_player video source");
-  source[0].src=filename + ".mp4";
-  source[1].src=filename + ".m3u8";
-  source[2].src=filename + ".webm";
   video.load();
   video.play();
+  
+    
+  filename=videotarget/*.substr(0,videotarget.lastIndexOf('.'))||videotarget*/;
+  source=document.querySelectorAll("#video_player video source");
+  source[0].src=filename/* + ".mp4"*/;
+  /*source[1].src=filename + ".m3u8";
+  source[2].src=filename + ".webm";*/
+  
+  
+  
 }
