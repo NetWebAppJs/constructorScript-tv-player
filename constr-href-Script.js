e88e6b9e972686f02971ddb126b6d584616bbd9d
Andrew Berlin <78539122+berlinandrew@users.noreply.github.com>
2021-10-22 09:21:49 +0300

    Update constr-href-Script.js

--- constr-href-Script.js
+++ constr-href-Script.js
@@ -8,7 +8,7 @@ bodyPoster.style.backgroundImage = 'url(https://64.media.tumblr.com/6baae01ac6fe
 let channels = {
   nasVrem:"https://rfe-lh.akamaihd.net/i/rfe_tvmc5@383630/master.m3u8",
   minIdey: "https://live-minidey.cdnvideo.ru/minidey/minidey.sdp/playlist.m3u8",
-  univSmotr: "https://live-minidey.cdnvideo.ru/minidey/minidey.sdp/playlist.m3u8",
+  univSmotr: "https://cdn.universmotri.ru/live/hq.sdp/playlist.m3u8",
   triAngel:"https://hls.tv.3angels.ru/stream/HQ.m3u8",
   muzTv:"https://rutv.gcdn.co/streams/1410_95/playlist.m3u8",
   rtDoc:"https://rtmp.api.rt.com/hls/rtdru.m3u8",
